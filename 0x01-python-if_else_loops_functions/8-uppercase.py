08fa0b84bec847353b390003da9b6bc78f9fe366
oussama laamri <laamri2002@gmail.com>
2023-08-09 14:50:12 +0000

    i fix simpl prblm

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,15 +1,8 @@
 #!/usr/bin/python3
-'''
-uppercase - function that prints a string in uppercase followed by a new line
-
-@str = string that will be uppercase
-'''
-
-
 def uppercase(str):
     for c in str:
-        unicd = chr(ord(c))
+        unicd = ord(c)
         if (unicd in range(97, 123)):
-            unicd = chr(unicd - 32)
-        print("{}".format(unicd), end='')
+            unicd = unicd - 32
+        print("{}".format(chr(unicd)), end='')
     print()
