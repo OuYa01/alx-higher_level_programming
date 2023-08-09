d12be4425de96aa29a4712a4188abe6aa141a25d
oussama laamri <laamri2002@gmail.com>
2023-08-09 14:31:55 +0000

    i write a function that print string in upper case

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''
+uppercase - function that prints a string in uppercase followed by a new line
+
+@str = string that will be uppercase
+'''
+
+
+def uppercase(str):
+    for c in str:
+        unicd = ord(c)
+        '#check if the char is lower case 97 = a and 122 = z'
+        if (unicd in range(97, 123)):
+            unicd = unicd - 32
+        print("{}".format(chr(unicd)), end='')
+
+    print()
