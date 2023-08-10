7e61fca0c1832f820f5aa8fa24cb2ec42af99e9d
oussama laamri <laamri2002@gmail.com>
2023-08-10 19:25:14 +0000

    i fix a simple fault

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,7 @@ count = len(sys.argv)
 if (count == 1):
     print("{} arguments.".format(count - 1))
 elif (count == 2):
-    print("{} argument".format(count - 1))
+    print("{} argument:".format(count - 1))
 else:
     print("{} arguments:".format(count - 1))
 for i in range(1, count):
