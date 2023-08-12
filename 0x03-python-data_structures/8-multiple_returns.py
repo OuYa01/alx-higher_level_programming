cf5a8ec4bad329fc8faf1e6ea8e091438bd98803
oussama laamri <laamri2002@gmail.com>
2023-08-12 23:44:22 +0000

    i add afnct thats has More returns!

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    '''
+    multiple_returns - function that returns a tuple with
+    the length of a string and its first character
+
+    @sentence:"string we want to return the len and the first char from it
+
+    Return: len of string and first char of string
+    '''
+    return (len(sentence), sentence[0])
