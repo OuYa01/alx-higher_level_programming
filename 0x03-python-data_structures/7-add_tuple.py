9beae51cdeab5ccf4f055f41fab09fda5aba8636
oussama laamri <laamri2002@gmail.com>
2023-08-12 20:35:26 +0000

    i fix a error

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -8,6 +8,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
 
     Return: the sum of tuple_a and tuple_b
     '''
+    sum_tuples = ()
     if (len(tuple_a) < 1):
         a1, a2 = 0, 0
     elif (len(tuple_a) < 2):
@@ -26,5 +27,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
         b1 = int(tuple_b[0])
         b2 = int(tuple_b[1])
 
-    sum_of_tuples = (a1+b1, b2+a2)
-    return (sum_of_tuples)
+    sum_tuples = a1 + b1, a2+ b2
+    return (sum_tuples)
