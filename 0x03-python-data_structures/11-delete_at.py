6d92733b249c7963d266305519e576acf8e02672
oussama laamri <laamri2002@gmail.com>
2023-08-13 00:25:10 +0000

    fnct that remove a element from string

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -8,8 +8,7 @@ def delete_at(my_list=[], idx=0):
 
     Return: the nwe list
     '''
-    if (idx < 0 or idx > len(my_list)):
-        return (my_list)
-    rm = my_list[idx]
-    my_list.remove(rm)
+    if (idx < 0 or idx > len(my_list)) or my_list is not None:
+        rm = my_list[idx]
+        my_list.remove(rm)
     return (my_list)
