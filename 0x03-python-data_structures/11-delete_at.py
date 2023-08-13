4d1c60cf10ed12ad4a402ca9c75c6f3cd1420b96
oussama laamri <laamri2002@gmail.com>
2023-08-13 00:23:04 +0000

    fnct that remove a element from string

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    '''
+    delet_at - function that delete a item in specific position in a list
+
+    @my_list: the list we work on
+    @idx: the position of the item we want to remove
+
+    Return: the nwe list
+    '''
+    if (idx < 0 or idx > len(my_list)):
+        return (my_list)
+    rm = my_list[idx]
+    my_list.remove(rm)
+    return (my_list)
