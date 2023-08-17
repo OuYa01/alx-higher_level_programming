eada91a869c68c71806a190d590ced4fd18fb08c
oussama laamri <laamri2002@gmail.com>
2023-08-17 13:31:36 +0000

    i add a comment to my code

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
+    '''
+    search_replace - function that replaces all occurrences of
+    an element by another in a new list
+
+    @my_list:  is the initial list
+    @search: is the element to replace in the list
+    @replace: is the new element
+
+    Return: new_list if the element we search its in the list
+            my_list if not
+    '''
     new_list = []
     if search in my_list:
         for i in my_list:
