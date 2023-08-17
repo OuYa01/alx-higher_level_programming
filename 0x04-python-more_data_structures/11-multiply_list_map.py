9bd32a81f71a48fb60c580c15c42bda9a5dcb190
oussama laamri <laamri2002@gmail.com>
2023-08-17 19:47:51 +0000

    i fix a probleme

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
 
 def multiply_list_map(my_list=[], number=0):
-    mul = list(map(lambda n: n * number, my_list))
-    return (mul)
+    '''
+    multiply_list_map - function that returns a list with all
+    values multiplied by a number
+
+    @my_list: The initial list
+    @number: Each value should be multiplied by this number
+
+    Return: the list with all values multiplied by a number
+    '''
+    return (list(map(lambda n: n * number, my_list)))
