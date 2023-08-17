3585dc3e21af8cbe2916cc493bd853ab070ff60c
oussama laamri <laamri2002@gmail.com>
2023-08-17 13:27:35 +0000

    i fix some errors

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -5,8 +5,9 @@ def search_replace(my_list, search, replace):
     if search in my_list:
         for i in my_list:
             new_list.append(i)
-        s = new_list.index(search)
-        new_list[s] = replace
+        for n in range(len(new_list)):
+            if new_list[n] == search:
+                new_list[n] = replace
         return (new_list)
     else:
         return (my_list)
