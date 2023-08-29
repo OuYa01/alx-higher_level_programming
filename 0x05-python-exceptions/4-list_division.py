16fd16c5bb39f48ea60cd39d1c8c87e896095a3e
oussama laamri <laamri2002@gmail.com>
2023-08-29 21:08:39 +0000

    i fix a error

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -12,7 +12,7 @@ def list_division(my_list_1, my_list_2, list_length):
             print("division by 0")
             result = 0
         except (IndexError):
-            print("out of the range")
+            print("out of range")
             result = 0
 
         finally:
