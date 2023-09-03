1aa73e9374d8c6be50bbee81cfb83f2a2aebbc0a
oussama laamri <laamri2002@gmail.com>
2023-09-03 13:13:47 +0000

    i add a calss thta has a privet attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+The module defines an class Square
+"""
+
+
+class Square:
+    """
+    This is a class has a privet attribute
+    """
+    def __init__(self, size: int):
+        self.__size = size
