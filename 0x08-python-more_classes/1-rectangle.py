7e5a059bbc128524adbf2a0c3d6faf7000fe58d0
oussama laamri <laamri2002@gmail.com>
2023-09-06 19:54:26 +0000

    i add two methods width and height

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+"""
+This modul define a class Rectangle
+"""
+
+
+class Rectangle:
+    """
+    This calss represnt a Rectangle
+    """
+
+    def __init__(self, width, height):
+        """
+        Initializes a Rectangle instance with give it a width and height
+        """
+        self.__height = height
+        self.__width = width
+
+    @property
+    def width(self):
+        """
+        Return:
+            int: width
+        """
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        """
+        Set the width of rectangle
+
+        Args:
+        value (int): the value of width
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """
+        Return:
+            int: height
+        """
+        return (self.__height)
+
+    @width.setter
+    def height(self, value):
+        """
+        Set the height of rectangle
+
+        Args:
+            value (int): the value of height
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
