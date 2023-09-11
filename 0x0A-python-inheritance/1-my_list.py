b5745b02f05c4de2e05290c204f22ec9ddf49fa2
oussama laamri <laamri2002@gmail.com>
2023-09-11 21:03:57 +0000

    another test

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,28 +1,13 @@
 #!/usr/bin/python3
 """ Creating Mylist class """
 
-class List:
-    """
-    List class
-    """
 
-    def __init__(self):
-        self.list1 = []
-    def append(self, item):
-        self.list1.append(item)
-    def print_stored(self):
-        print(self.list1)
-
-class MyList(List):
+class MyList(list):
     """
     MyList class that inherits from list
     """
-
-    def __init__(self):
-        super().__init__()
-
     def print_sorted(self):
-        self.list1.sort()
-        print(self.list1)
-    def __str__(self):
-        return (str(self.list1))
+        """
+        Prints the list in sorted order
+        """
+        print(sorted(self))
