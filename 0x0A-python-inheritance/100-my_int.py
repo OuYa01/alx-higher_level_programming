f05f38a55035a449276fd9c2b6c6ee1dae062518
oussama laamri <laamri2002@gmail.com>
2023-09-12 01:12:05 +0000

    i add  class MyInt that inherits from int

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""A class"""
+
+
+class MyInt(int):
+    """ MyInt class"""
+
+    def __init__(self, value):
+        self.value = value
+
+    def ch(self):
+        if self.value == value:
+            return (False)
+        else:
+            return (True)
+
+    def __str__(self):
+        return f"{self.value}"
