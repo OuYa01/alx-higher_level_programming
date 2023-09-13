072c5b6ed7c5d87d06bbc86adea3e8c0a27bb510
oussama laamri <laamri2002@gmail.com>
2023-09-13 01:27:49 +0000

    i fix a er

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,5 +6,4 @@ def read_file(filename=""):
     """function that reads a text file (UTF8) and prints it to stdout"""
 
     with open(filename) as file:
-        print(file.read())
-        file.close
+        print(file.read(), end="")
