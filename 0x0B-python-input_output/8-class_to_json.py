3812ffba9f8b9cd08524ff01c9b19a2d141661e0
oussama laamri <laamri2002@gmail.com>
2023-09-14 23:54:06 +0000

    i fix it

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -8,26 +8,4 @@ def class_to_json(obj):
     with simple data structure (list, dictionary, string, integer and boolean)
     for JSON serialization of an object
     """
-    my_dir = {}
-    attributes = dir(obj)
-
-    for name in attributes:
-
-        if name.startswith("__"):
-            continue
-
-        att_val = getattr(obj, name)
-
-        if isinstance(att_val, (int, str, bool)):
-            my_dir[name] = att_val
-
-        elif isinstance(att_val, (list)):
-            my_dir[att_val] = []
-
-            for item in att_val:
-                my_dir[name].append(item)
-        elif isinstance(att_val, dict):
-            my_dir[name] = {}
-            for key, value in att_value.items():
-                my_dir[name][key] = value
-    return my_dir
+    return obj.__dict__
