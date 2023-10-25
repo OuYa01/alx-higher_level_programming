6dbdf6958db1f304f1ef280b1c14cf49c572857a
Oussama Laamri <113089521+OuYa01@users.noreply.github.com>
2023-10-25 19:42:36 +0100

    Delete 0x0B-python-input_output directory

--- 0x0B-python-input_output/0-read_file.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""Creating a function"""
-
-
-def read_file(filename=""):
-    """function that reads a text file (UTF8) and prints it to stdout"""
-
-    with open(filename) as file:
-        print(file.read(), end="")
--- 0x0B-python-input_output/6-load_from_json_file.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""Creating a file"""
-import json
-
-
-def load_from_json_file(filename):
-    """function that creates an Object from a “JSON file”"""
-    with open(filename, "r") as file:
-        return json.load(file)
