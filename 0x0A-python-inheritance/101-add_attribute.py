f52ac4a39a5719c17ee2eeabb3d3a370375fafb2
Oussama Laamri <113089521+OuYa01@users.noreply.github.com>
2023-10-25 19:39:31 +0100

    Delete 0x0A-python-inheritance directory

--- 0x0A-python-inheritance/101-add_attribute.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""creating a function"""
-
-
-def add_attribute(obj, name, value):
-    """ add_attribute function """
-    if not hasattr(obj, "__dict__"):
-        raise TypeError("can't add new attribute")
-    setattr(obj, name, value)
